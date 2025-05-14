caaac87fc749bb4e044119de0e2877b8cf087b15
Camilo López <camiloj.lopez79@gmail.com>
2025-05-14 18:34:36 -0400

    ExpoRouter: index y _layout

---
+++ app/index.tsx
@@ -0,0 +1,16 @@
+// React Native
+import { 
+  View,
+  Text
+} from 'react-native';
+
+
+const CalculatorApp = () => {
+  return (
+    <View>
+      <Text>CalculatorApp</Text>
+    </View>
+  );
+}
+
+export default CalculatorApp;
