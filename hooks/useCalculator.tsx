a6f9133d6130efc53db99831286224b176880ba0
Camilo López <camiloj.lopez79@gmail.com>
2025-05-16 01:15:21 -0400

    Construir número

--- hooks/useCalculator.tsx
+++ hooks/useCalculator.tsx
@@ -12,29 +12,49 @@ enum Operator {
   divide = '/',
 }
 
-  export const useCalculator = () => {
-    const [ formula, setFormula ] = useState( '0' );
-    const [ number, setNumber ] = useState( '0' );
-    const [ prevNumber, setPrevNumber ] = useState( '0' );
+export const useCalculator = () => {
+  const [ formula, setFormula ] = useState( '0' );
+  const [ number, setNumber ] = useState( '0' );
+  const [ prevNumber, setPrevNumber ] = useState( '0' );
 
-    const lastOperation = useRef<Operator>( undefined );
+  const lastOperation = useRef<Operator>( undefined );
 
-    useEffect( () => {
-      setFormula( number );
+  useEffect( () => {
+    setFormula( number );
 
-      return () => {}
-    }, [ number ] );
+    return () => {}
+  }, [ number ] );
 
-    const buildNumber = ( numberString: string ) => {
-      console.log( numberString );
+  const buildNumber = ( numberString: string ) => {
+    // Validar si ya existe el punto decimal
+    if ( number.includes('.') && numberString === '.' )
+      return;
+
+    if ( number.startsWith('0') || number.startsWith('-0') ) {
+      if ( numberString === '.' )
+        return setNumber( number + numberString );
+
+      // Evaluar si es otro cero y no hat punto
+      if ( numberString === '0' && number.includes('.') )
+        return setNumber( number + numberString );
+
+      // Evaluar si es diferente de cero, no hay punto y es el primer número
+      if ( numberString !== '0' && !number.includes('.') )
+        return setNumber( numberString ) 
+
+      if ( numberString === '0' && !number.includes('.') )
+        return;
     }
 
-    return {
-      // Props
-      formula,
-      number,
-      prevNumber,
-      // Methods
-      buildNumber
-    }
+    setNumber( number + numberString );
   }
+
+  return {
+    // Props
+    formula,
+    number,
+    prevNumber,
+    // Methods
+    buildNumber
+  }
+}
