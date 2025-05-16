31f8e63e438d6b3f7a5d5b7ca6ead6fb4ae45e82
Camilo López <camiloj.lopez79@gmail.com>
2025-05-16 01:02:37 -0400

    Lógica de la calculadora - useCalculator

--- app/index.tsx
+++ app/index.tsx
@@ -4,16 +4,22 @@ import { CustomText } from '@/components/CustomText';
 import { CalculatorButton } from '@/components/CalculatorButton';
 import { Colors } from '@/constants/Colors';
 import { globalStyles } from '@/styles/global-styles';
+import { useCalculator } from '@/hooks/useCalculator';
 
 
 const CalculatorApp = () => {
+  const {
+    formula,
+    buildNumber
+  } = useCalculator();
+
   return (
     <View style={ globalStyles.calculatorContainer }>
       <View style={{ paddingHorizontal: 30, paddingBottom: 20 }}>
         <CustomText
           variant='h1'
         >
-          50 x 50
+          { formula }
         </CustomText>
 
         <CustomText
@@ -55,17 +61,17 @@ const CalculatorApp = () => {
       <View style={ globalStyles.row }>
         <CalculatorButton
           label='7'
-          onPress={() => console.log( '7' )}
+          onPress={() => buildNumber( '7' )}
         />
 
         <CalculatorButton
           label='8'
-          onPress={() => console.log( '8' )}
+          onPress={() => buildNumber( '8' )}
         />
 
         <CalculatorButton
           label='9'
-          onPress={() => console.log( '9' )}
+          onPress={() => buildNumber( '9' )}
         />
 
         <CalculatorButton
@@ -78,17 +84,17 @@ const CalculatorApp = () => {
       <View style={ globalStyles.row }>
         <CalculatorButton
           label='4'
-          onPress={() => console.log( '4' )}
+          onPress={() => buildNumber( '4' )}
         />
 
         <CalculatorButton
           label='5'
-          onPress={() => console.log( '5' )}
+          onPress={() => buildNumber( '5' )}
         />
 
         <CalculatorButton
           label='6'
-          onPress={() => console.log( '6' )}
+          onPress={() => buildNumber( '6' )}
         />
 
         <CalculatorButton
@@ -101,17 +107,17 @@ const CalculatorApp = () => {
       <View style={ globalStyles.row }>
         <CalculatorButton
           label='1'
-          onPress={() => console.log( '1' )}
+          onPress={() => buildNumber( '1' )}
         />
 
         <CalculatorButton
           label='2'
-          onPress={() => console.log( '2' )}
+          onPress={() => buildNumber( '2' )}
         />
 
         <CalculatorButton
           label='3'
-          onPress={() => console.log( '3' )}
+          onPress={() => buildNumber( '3' )}
         />
 
         <CalculatorButton
@@ -125,12 +131,12 @@ const CalculatorApp = () => {
         <CalculatorButton
           label='0'
           doubleSize
-          onPress={() => console.log( '0' )}
+          onPress={() => buildNumber( '0' )}
         />
 
         <CalculatorButton
           label='.'
-          onPress={() => console.log( '.' )}
+          onPress={() => buildNumber( '.' )}
         />
 
         <CalculatorButton
